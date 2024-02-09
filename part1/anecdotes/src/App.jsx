c720c1a940b3998ba490c6c1235b70eeb83eabbe
Pedro Paças <pndspacas@gmail.com>
2024-02-08 22:59:22 -0800

    finished

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -1,8 +1,46 @@
+import { useState } from 'react';
+
 function App() {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.',
+  ];
+
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length));
+  };
+
+  const handleClickVote = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
+  const mostVotedIndex = votes.indexOf(Math.max(...votes));
+
   return (
     <>
       <div>
-        <h2>hey</h2>
+        <h2>Anecdote of the Day</h2>
+        <p>{anecdotes[selected]}</p>
+        <p>has {votes[selected]} votes</p>
+        <button onClick={handleClick}>next anecdote</button>
+        <button onClick={handleClickVote}>vote</button>
+        {votes[mostVotedIndex] > 0 && (
+          <>
+            <h2>Anecdote with most votes</h2>
+            <p>{anecdotes[mostVotedIndex]}</p>
+            <p>has {votes[mostVotedIndex]} votes</p>
+          </>
+        )}
       </div>
     </>
   );
